7968b5bbab1c5306bc2dae164c4eccfd43c448c8
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-19 21:44:54 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,1 +1,33 @@
-cat rakesh
+#!/bin/bash
+
+# This is a comment
+echo "Hello, world!"  # This prints a message
+
+# Variables
+name="John"
+echo "Hello, $name"
+
+# If-else statement
+if [ $name == "John" ]; then
+  echo "The name is John"
+else
+  echo "The name is not John"
+fi
+
+# Loop (for loop example)
+for i in {1..5}
+do
+  echo "This is iteration number $i"
+done
+
+# Creating a directory and changing to it
+mkdir my_directory
+cd my_directory
+echo "You are now in $(pwd)"
+
+# Create a simple file and write to it
+echo "This is a simple file" > myfile.txt
+cat myfile.txt
+
+# End of script
+echo "Script execution completed!"
