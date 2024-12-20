180f1d6289f30e09cb74df8f0447278e11f6d0e2
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-20 11:08:06 +0530

    new

--- new.sh
+++ new.sh
@@ -1,17 +1,3 @@
 #!/bin/bash
 
-# Define the folder and file names
-folder_name="my_folder"
-file1_name="file1.txt"
-file2_name="file2.txt"
-
-# Create the folder
-mkdir -p "$folder_name"
-
-# Create file1 and add data
-echo "This is the content of file1." > "$folder_name/$file1_name"
-
-# Create file2 and add data
-echo "This is the content of file2." > "$folder_name/$file2_name"
-
-echo "Folder and files have been created successfully!"
+touch new-jenkins-project
