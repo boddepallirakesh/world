30a723cff758757333a6808c26eb6757ebd436fc
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-20 18:55:16 +0530

    new

--- new.py
+++ new.py
@@ -4,7 +4,6 @@ def check_even_odd(number):
         return "Even"
     else:
         return "Odd"
-
 # Loop through numbers 1 to 10 and check if they are even or odd
 for i in range(1, 11):
     result = check_even_odd(i)
