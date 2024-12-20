272e86fb535a6524f7c8bb08ab08d4769b45c494
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-20 11:58:41 +0530

    new05

--- new.sh
+++ new.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
-mkdir new
-cd new
-cat rakesh
-hello from rakesh
-thank you
+#!/bin/bash
+
+# Define folder and file names
+folder_name="my_folder"
+file1="file1.txt"
+file2="file2.txt"
+
+# Create the folder
+mkdir "$folder_name"
+
+# Create the files inside the folder
+touch "$folder_name/$file1" "$folder_name/$file2"
+
+echo "Folder '$folder_name' and files '$file1' and '$file2' created successfully."
 
