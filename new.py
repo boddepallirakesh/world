d2910fffddb7cf355a5d9dde907ff2abf7376c99
boddepallirakesh <141721486+boddepallirakesh@users.noreply.github.com>
2024-12-20 18:52:28 +0530

    new

---
+++ new.py
@@ -0,0 +1,12 @@
+# Function to check if a number is even or odd
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Loop through numbers 1 to 10 and check if they are even or odd
+for i in range(1, 11):
+    result = check_even_odd(i)
+    print(f"Number {i} is {result}.")
+
